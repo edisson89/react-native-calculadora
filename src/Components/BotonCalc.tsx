fd3a5a04935151754c955807e4c147c19c9da004
edisson89 <edisson.12@hotmail.com>
2023-04-03 18:51:59 -0500

    cambios

---
+++ src/Components/BotonCalc.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {styles} from '../Theme/appTheme';
+
+interface Props {
+  texto: string;
+  color?: string;
+  ancho?: boolean;
+  accion: (numero: string) => void;
+}
+export const BotonCalc = ({
+  texto,
+  color = '#2D2D2D',
+  ancho = false,
+  accion,
+}: Props) => {
+  return (
+    <TouchableOpacity onPress={() => accion(texto)}>
+      <View
+        style={{
+          ...styles.boton,
+          backgroundColor: color,
+          width: ancho ? 180 : 80,
+        }}>
+        <Text
+          style={{
+            ...styles.botonTexto,
+            color: color === '9B9B9B' ? 'black' : 'white',
+          }}>
+          {texto}
+        </Text>
+      </View>
+    </TouchableOpacity>
+
+
+
+  );
+};
